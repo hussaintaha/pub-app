376ebd83be8fb38060a4691409754acfd6dc795f
aditya-burse <vowelweb94@gmail.com>
2025-06-19 13:50:56 +0530

    temp chnage - script installation

--- app/routes/api.v1.widget-script.installation.js
+++ app/routes/api.v1.widget-script.installation.js
@@ -1,5 +1,20 @@
 import { Script } from "../models";
 
+export const loader = async ({ request }) => {
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  }
+
+  return new Response(null, { status: 405 }); 
+};
+
 export const action = async ({ request }) => {
   try {
     console.log("Webhook triggered. /api/v1/widget-script/installation");
