820ba9d8236eccb4c6d50526f94e15eaac574419
aditya-burse <vowelweb94@gmail.com>
2025-06-20 16:44:01 +0530

    update unsubscribe webhook

--- app/routes/webhooks.app_subscriptions.update.jsx
+++ app/routes/webhooks.app_subscriptions.update.jsx
@@ -1,51 +1,71 @@
 // Subscription update webhook route
 
 import { authenticate } from "../shopify.server";
-import {Subscription} from '../models'
+import { Activity, AnalyticsUpdate, ProductSyncStatus, Script, Subscription } from "../models";
 
 const at = "webhook.app_subscriptions.update.jsx";
 
-const processWebhook = async({shop, payload})=>{
-    try {
-        if(shop && payload){
-            const subscriptionDataFound = await Subscription.findOne({shop})
-            if(subscriptionDataFound && payload.app_subscription.status !== "DECLINED"){
-                if(payload.app_subscription.status !== "CANCELLED"){
-                    await Subscription.findOneAndUpdate({shop}, {$set:{
-                        graphQlID: payload.app_subscription.admin_graphql_api_id,
-                        status: payload.app_subscription.status,
-                        planName: payload.app_subscription.name,
-                    }})
-                }else if(payload.app_subscription.status === "CANCELLED"){
-                    await Subscription.findOneAndUpdate({shop}, {$set:{
-                        graphQlID: payload.app_subscription.admin_graphql_api_id,
-                        status: "INACTIVE",
-                        planName: "Free Plan",
-                    }})
-                }
-            }
+const processWebhook = async ({ shop, payload }) => {
+  try {
+    if (shop && payload) {
+      const subscriptionDataFound = await Subscription.findOne({ shop });
+      if (
+        subscriptionDataFound &&
+        payload.app_subscription.status !== "DECLINED"
+      ) {
+        if (payload.app_subscription.status !== "CANCELLED") {
+          await Subscription.findOneAndUpdate(
+            { shop },
+            {
+              $set: {
+                graphQlID: payload.app_subscription.admin_graphql_api_id,
+                status: payload.app_subscription.status,
+                planName: payload.app_subscription.name,
+              },
+            },
+          );
+        } else if (payload.app_subscription.status === "CANCELLED") {
+          await Subscription.findOneAndUpdate(
+            { shop },
+            {
+              $set: {
+                graphQlID: payload.app_subscription.admin_graphql_api_id,
+                status: "INACTIVE",
+                planName: "Free Plan",
+              },
+            },
+          );
+
+          await Activity.findOneAndDelete({ shopify_domain: shop });
+          await AnalyticsUpdate.findOneAndDelete({ shopify_domain: shop });
+          await ProductSyncStatus.findOneAndDelete({ shop });
+          await Script.findOneAndDelete({ shop });
         }
-        
-    } catch (error) {
-        console.log(`Subscription error ${at}`);
-
+      }
     }
-}
+  } catch (error) {
+    console.log(`Subscription error ${at}`);
+  }
+};
 
-export const action = async({request})=>{
-    try {
-        const { shop, payload, } = await authenticate.webhook(request);
-        console.log('Received subscription webhook for shop:', {shop, payload});
+export const action = async ({ request }) => {
+  try {
+    const { shop, payload } = await authenticate.webhook(request);
+    console.log("Received subscription webhook for shop:", { shop, payload });
 
-        processWebhook({ payload, shop });
+    processWebhook({ payload, shop });
 
-        return new Response({success: true})
-    } catch (error) {
-        if(error instanceof Error){
-            console.log(`An error occurred while subscription: ${error.message}`);
-        }else{
-            console.log('An unknown error occurred.');
-        }
-        return new Response({status: 500, success: false, error:"Internal server error."})
+    return new Response({ success: true });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`An error occurred while subscription: ${error.message}`);
+    } else {
+      console.log("An unknown error occurred.");
     }
-}
+    return new Response({
+      status: 500,
+      success: false,
+      error: "Internal server error.",
+    });
+  }
+};
