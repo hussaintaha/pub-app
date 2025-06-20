c1bdc75dd3ff7e1dbba90883bac00dc711890203
aditya-burse <vowelweb94@gmail.com>
2025-06-20 15:56:24 +0530

    update uninstall webhook

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -1,18 +1,34 @@
 // Webhook requests can trigger multiple times and after an app has already been uninstalled.
-  // If this webhook already ran, the session may have been deleted previously.
+// If this webhook already ran, the session may have been deleted previously.
 
 import { authenticate } from "../shopify.server";
-import { ShopifySession } from "../models";
+import {
+  Activity,
+  AnalyticsUpdate,
+  ProductSyncStatus,
+  Script,
+  ShopifySession,
+  Subscription,
+} from "../models";
 
 export const action = async ({ request }) => {
   try {
     const { shop, topic } = await authenticate.webhook(request);
     console.log(`Received ${topic} webhook for ${shop}`);
 
-    await ShopifySession.findOneAndDelete({shop})
+    const subscription = await Subscription.findOne({ shop });
+
+    if (!subscription || subscription?.status === "INACTIVE" ) {
+      await Activity.findOneAndDelete({ shop });
+      await AnalyticsUpdate.findOneAndDelete({ shop });
+      await ProductSyncStatus.findOneAndDelete({ shop });
+      await Script.findOneAndDelete({ shop });
+      await Subscription.findOneAndDelete({shop})
+    }
+
+    await ShopifySession.findOneAndDelete({ shop });
 
     return new Response(null, { status: 200 });
-
   } catch (err) {
     console.error("Error processing APP_UNINSTALLED webhook:", err);
   }
